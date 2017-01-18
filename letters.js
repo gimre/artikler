2050f8ec8e9f40cf3a965a2da4836f7926a567f3
Gabriel Imre <gabrielimre@work.local>
2017-01-18 09:36:43 +0100

    added propert diacritic handling

---
+++ letters.js
@@ -0,0 +1,15 @@
+class Lexics {
+    static get baseLetters( ) {
+        return {
+            'ä': 'a',
+            'Ä': 'a',
+            'ë': 'e',
+            'Ë': 'e',
+            'ö': 'o',
+            'Ö': 'o',
+            'ü': 'u',
+            'Ü': 'u',
+            'ß': 'ss'
+        }
+    }
+}
